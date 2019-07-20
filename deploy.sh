fc6d83b1cbf3224ddd827a285b186deaecde96f8
JoHoYoung <whghdud17@naver.com>
2019-07-20 15:54:56 +0900

    no message

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ then
     java -jar -Dspring.profiles.active=${ENV} ./target/myapp-0.0.1.jar
 elif [ $ENV == "prod" ]
 then
-    mvn package spring-boot:repackage -Dmaven.test.skip=true --add-modules java.xml.bind
+    mvn package spring-boot:repackage -Dmaven.test.skip=true
     docker -H ${HOST} rm -f groot
     docker -H ${HOST} image rm groot:0.1
     docker -H ${HOST} build --no-cache -t groot:0.1 .
